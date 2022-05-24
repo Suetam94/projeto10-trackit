e31328ff1fbb57b4407b6570ba103541b699d6c7
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-05-24 13:54:44 -0300

    initial config and global style

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: 0;
+    text-decoration: none;
+    list-style: none;
+  }
+
+  :root {
+    --general-blue: #52B6FF;
+    --darker-blue: #126BA5;
+    --general-border-style: 1px solid #D4D4D4;
+    --border-radius: 5px;
+    --general-white: #FFF;
+    --placeholder-color: #DBDBDB;
+    --calendar-gray-color: #CFCFCF;
+    --inactive-check-color: #e7e7e7;
+    --active-check-color: #8FC549;
+    --body-background-color: #e7e7e7;
+  }
+  
+  body {
+    font-family: 'Lexend Deca', sans-serif;
+    background: var(--body-background-color);
+  }
+  
+`;
