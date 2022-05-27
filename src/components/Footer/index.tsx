3f81db526549157f8c0b7a633e205506c762dbd5
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-05-27 18:44:27 -0300

    making the footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -15,20 +15,22 @@ export function Footer() {
         <Link href={"/habitos"}>
           <FooterSpan>Hábitos</FooterSpan>
         </Link>
-        <TodayCircle >
-          <CircularProgressbar
-            styles={buildStyles({
-              backgroundColor: "#52B6FF",
-              pathColor: "#FFF",
-              trailColor: "#52B6FF",
-              textColor: "#FFF",
-            })}
-            value={10}
-            background={true}
-            backgroundPadding={6}
-            text={"Hoje"}
-          />
-        </TodayCircle>
+        <Link href={"/hoje"}>
+          <TodayCircle>
+            <CircularProgressbar
+              styles={buildStyles({
+                backgroundColor: "#52B6FF",
+                pathColor: "#FFF",
+                trailColor: "#52B6FF",
+                textColor: "#FFF",
+              })}
+              value={10}
+              background={true}
+              backgroundPadding={6}
+              text={"Hoje"}
+            />
+          </TodayCircle>
+        </Link>
         <Link href={"/historico"}>
           <FooterSpan>Histórico</FooterSpan>
         </Link>
