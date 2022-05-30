a56ba0b5cad7784e7d4fdf43bf015f593cfed6d9
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-05-30 15:10:47 -0300

    percent today habits completed sentence

--- src/components/Today/index.tsx
+++ src/components/Today/index.tsx
@@ -15,6 +15,18 @@ import { ptBR } from "date-fns/locale";
 export function Today() {
   const { todayHabits } = useContext(HabitsContext);
 
+  function handleCalcPercent(): number {
+    const done = todayHabits
+      .map((habit) => {
+        if (habit.done) {
+          return habit;
+        }
+      })
+      .filter((habit) => habit);
+
+    return done.length / todayHabits.length;
+  }
+
   const date = new Date();
   const actualWeekday = format(date, "eee, dd/MM", { locale: ptBR });
 
@@ -23,8 +35,10 @@ export function Today() {
       <TodayContainer>
         <TitleContainer>
           <MainTitle>{actualWeekday}</MainTitle>
-          {todayHabits.length === 0 && (
+          {todayHabits.length === 0 ? (
             <SubTitle>Nenhum hábito concluído ainda</SubTitle>
+          ) : (
+            <SubTitle className={"active"}>{(handleCalcPercent() * 100).toFixed(0)}% dos hábitos concluídos</SubTitle>
           )}
         </TitleContainer>
         {todayHabits.map(
